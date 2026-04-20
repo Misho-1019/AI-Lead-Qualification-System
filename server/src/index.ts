8c023519d2a717ab8dca52e8dc6f9088cd503866
Misho1019 <mikailtorres99@gmail.com>
2026-04-20 20:22:10 +0300

    Introduce basic backend folder structure and health route

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+import healthRoute from "./routes/health.route";
+
 const app = express();
 const PORT: number = Number(process.env.PORT) || 3030;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-})
+app.use('/', healthRoute);
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
