f707f7908566c440fad17b99c6dbb30f0cb7820e
Misho1019 <mikailtorres99@gmail.com>
2026-04-28 03:00:08 +0300

    Rregister-error-handler-middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import healthRoute from "./routes/health.route";
 import leadRoute from "./routes/lead.route";
 import internalRoutes from "./routes/internal.route";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 const PORT: number = Number(process.env.PORT) || 3030;
@@ -16,5 +17,7 @@ app.use(express.json());
 app.use('/', healthRoute);
 app.use('/api/leads', leadRoute)
 app.use('/api/internal', internalRoutes)
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
